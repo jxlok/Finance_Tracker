c1aebabdd824c2d3432d65ec7267e4c33b976c71
Jason Lok <jason.lok@ucdconnect.ie>
2022-09-03 21:45:59 +0100

    Implemented the basic input and outputting of file data with minimal categorising at the moment

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,111 @@
+
+
 public class Transaction {
 
-    private int val;
+    private String type;
+    private String Product;
+    private String startedDate;
+    private String completedDate;
+    private String description;
+    private double Amount;
+    private double fee;
+    private String currency;
+    private String state;
+    private double balance;
+
+    public Transaction() {
+    }
+
+    public Transaction(String type, String product, String startedDate, String completedDate, String description, double amount, double fee, String currency, String state, double balance) {
+        this.type = type;
+        Product = product;
+        this.startedDate = startedDate;
+        this.completedDate = completedDate;
+        this.description = description;
+        Amount = amount;
+        this.fee = fee;
+        this.currency = currency;
+        this.state = state;
+        this.balance = balance;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getProduct() {
+        return Product;
+    }
+
+    public void setProduct(String product) {
+        Product = product;
+    }
+
+    public String getStartedDate() {
+        return startedDate;
+    }
+
+    public void setStartedDate(String startedDate) {
+        this.startedDate = startedDate;
+    }
+
+    public String getCompletedDate() {
+        return completedDate;
+    }
+
+    public void setCompletedDate(String completedDate) {
+        this.completedDate = completedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(double amount) {
+        Amount = amount;
+    }
+
+    public double getFee() {
+        return fee;
+    }
+
+    public void setFee(double fee) {
+        this.fee = fee;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
--- src/main.java
+++ src/main.java
@@ -1,4 +1,86 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class main {
 
+    //throws exception for if file not found
+    public static void main(String[] args) throws Exception {
 
+        //arraylist for income
+        ArrayList<Transaction> income = new ArrayList<>();
+
+        //arraylist for expenditure
+        ArrayList<Transaction> expenditure = new ArrayList<>();
+
+        //read from csv using Scanner
+        Scanner scanner = new Scanner(new File("resource/statement.csv"));
+        scanner.nextLine();
+        //use comma as delimiter
+
+        while(scanner.hasNext()){
+
+            scanner.useDelimiter("[,\n]");
+
+            //add to a new transaction
+            Transaction transaction = new Transaction();
+
+            //fill transaction with information from file
+            transaction.setType(scanner.next());
+            transaction.setProduct(scanner.next());
+            transaction.setStartedDate(scanner.next());
+            transaction.setCompletedDate(scanner.next());
+            transaction.setDescription(scanner.next());
+            transaction.setAmount(Double.parseDouble(scanner.next()));
+            transaction.setFee(Double.parseDouble(scanner.next()));
+            transaction.setCurrency(scanner.next());
+            transaction.setState(scanner.next());
+            transaction.setBalance(Double.parseDouble(scanner.next()));
+
+            //depending on first word in description, discern which category to put it in
+            String check = transaction.getDescription().split(" ")[0];
+            if(check.equals("To")){
+                expenditure.add(transaction);
+            }
+            else if(check.equals("From") || check.equals("Top-Up")){
+                income.add(transaction);
+            }
+            else{
+                expenditure.add(transaction);
+            }
+
+        }
+
+        //close scanner
+        scanner.close();
+
+        //output income
+        System.out.println("__Income__");
+        double totalIncome = 0.0;
+        for(Transaction transaction : income){
+
+            //sum up total income
+            totalIncome += transaction.getAmount();
+
+            System.out.println(transaction.getDescription() +" +" + transaction.getAmount() + "\n\t\t "+transaction.getBalance());
+        }
+
+        System.out.println("\n\n");
+
+        //output expenditures
+        System.out.println("__Expenditure__");
+        double totalExpenditure = 0.0;
+        for(Transaction transaction : expenditure){
+
+            //sum up total income
+            totalExpenditure += transaction.getAmount();
+            System.out.println(transaction.getDescription() +" " + transaction.getAmount() + "\n\t\t "+transaction.getBalance());
+        }
+
+        System.out.println("\n\n");
+
+        //output income and expenditure in rounded format
+        System.out.println("Total income: +" + Math.round(totalIncome*100)/100);
+        System.out.println("Total expenditure: " + Math.round(totalExpenditure*100)/100);
+    }
 }
